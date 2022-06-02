2eddcc00c6ff370bd0d86b607031bc9bdcea03c2
hieudzai <dinhvanhieu2002@gmail.com>
2022-06-02 15:07:34 +0700

    abbbbb

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
-const path = require('path')
-const express = require('express')
-var exphbs = require('express-handlebars')
-const morgan = require('morgan')
-const app = express()
-const port = 3000
+const path = require('path');
+const express = require('express');
+var exphbs = require('express-handlebars');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
 
-const route = require('./routes')
+const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 //template engine
-app.engine('hbs', exphbs.engine({
-    extname : '.hbs'
-}));
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'resources/views'))
+app.engine(
+    'hbs',
+    exphbs.engine({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 //HTTP logger
 // app.use(morgan('combined'))
 
 //Route init
-        route(app)
+route(app);
 
-        app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () =>
+    console.log(`Example app listening at http://localhost:${port}`),
+);
