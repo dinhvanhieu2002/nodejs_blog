8ad10fd586da7042a707c1cd0ff80dc72d39d4d8
hieudzai <dinhvanhieu2002@gmail.com>
2022-06-02 14:43:53 +0700

    Install prettier lint-staged husky

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 //template engine
@@ -15,9 +17,9 @@ app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
 
 //HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
-app.get('/', (req, res) => res.render('home'))
-app.get('/news', (req, res) => res.render('news'))
+//Route init
+        route(app)
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+        app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
